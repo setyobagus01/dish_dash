4737144c5821c6cda4e6f1b0f60079c2630a3495
Muhammad Rifki <69866073+rifki-sgnic@users.noreply.github.com>
2023-02-11 10:21:53 +0700

    Adding Screens Install react-native-svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Cart, Home } from "./presentation";
+import {
+  Cart,
+  Confirmation,
+  FoodList,
+  Home,
+  Payment,
+  Profile,
+} from "./presentation";
 
 export type StackParams = {
   Home: any;
   Cart: {
     item: string;
   };
+  Profile: undefined;
+  FoodList: undefined;
+  Payment: undefined;
+  Confirmation: undefined;
 };
 
 const Stack = createNativeStackNavigator<StackParams>();
@@ -23,6 +34,26 @@ const App = () => {
         <Stack.Screen
           name="Cart"
           component={Cart}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Profile"
+          component={Profile}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="FoodList"
+          component={FoodList}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Payment"
+          component={Payment}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Confirmation"
+          component={Confirmation}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
