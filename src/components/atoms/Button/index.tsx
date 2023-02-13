f481e02b2ed17bd08efcbcb90a30bb36716a7fa4
setyobagus01 <103160255+sobattechno@users.noreply.github.com>
2023-02-13 12:12:49 +0700

    Revert "Merge pull request #1 from setyobagus01/features/bottomnav"
    This reverts commit 8017f279f985c6741217c820cbb055e839e2dafe, reversing
    changes made to 4737144c5821c6cda4e6f1b0f60079c2630a3495.

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import React from "react";
-import { colors } from "../../../utils";
+import { colors } from "../../../utils/colors";
 
 interface ButtonProps {
   item: string;
